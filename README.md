c945f985ec4f23ab9223e5fefbd4ece50de6ff27
Oku <jdrodas76720@umanizales.edu.co>
2022-08-22 12:16:53 -0500

    App created but functionality doesn't work properly

--- README.md
+++ README.md
@@ -4,10 +4,15 @@ This repository will provide the host for all my code, for the react app and als
 
 ## Main goals
 
-- [ ] Using JSX build the app from scratch 
-- [ ] App Functionality using JavaScript
+- [x] Using JSX build the app from scratch 
+- [x] App Functionality using JavaScript
 - [ ] Styling the app using a CSS react framework to learn how to use it
 - [ ] Make API calls using axios for better underestanding and how is implemented in a real application
 - [ ] Using the following api to build all the **CRUD** functionality: [cat facts](https://catfact.ninja/fact)
+
+## Problems so far
+
+- [ ] React recommends to destruct the Object to display their child elements, _Need to search how to do it_
+- [ ] Consume the api, the data is displayed thanks to the use of a varible and using bracket notation i can display the elements
 
 > This is for learning propurses don't use for real implementations :)
--- axios-cat-app/src/App.js
+++ axios-cat-app/src/App.js
@@ -1,32 +1,77 @@
 import axios from "axios";
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const CATURL = "https://catfact.ninja/fact";
-const cat = {
-  "name": "Lucifer",
-  "color": "black"
-}
+
+// Hard coding a facts for testing
+const newFact = {
+  fact: "The cat is a domestic species of small carnivorous mammal",
+  length: 57
+};
+
+const updatedFact = {
+  fact: "Domestic cats are valued by humans for companionship and their ability to kill rodents",
+  length: 86 
+};
 
 export default function App() {
-  const [fact, setFact] = React.useState(null);
-
-  React.useEffect(() => {
+  const [data, setData] = useState(null);
+  //const [fact, setFact] = useState(null);
+  
+  // Check the connection with the API
+  useEffect(() => {
     axios.get(CATURL).then((response) => {
-      setFact(response.data)
+      setData(response.data)
     })
   }, []);
 
-  if(!fact){
+  // This is an object {fact: "", length: ""}
+  let dataFromApi = data; 
+  
+  //// Create a fact
+  const createFact = () => {
+    axios
+      .post(CATURL, newFact).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+  }
+
+  // Update a fact
+  const updateFact = () => {
+    axios
+      .put(CATURL, updatedFact).then((response) => {
+        setData(response.data)
+      })
+  }
+
+  // Delete a fact
+  const deleteFact = () => {
+    axios
+      .delete(CATURL).then(() => {
+        alert("The post has been deleted, ^-^ ...");
+        setData(null)
+      })
+  }
+
+
+  // Control flow from the app
+  if(!dataFromApi){
     return (
-      <p>No response</p>
+      alert(`Error, Can't Connect with: ${CATURL}`)
     )
   } else {
-      return (
+      return(
         <div>
-          <p>{fact}</p>
+          <h1>Cat facts</h1>
+          <h2>Did you know !!!</h2>
+          <p>{dataFromApi.fact}</p>
+          <button onClick={createFact}>Create Fact</button>
+          <button onClick={updateFact}>Update Fact</button>
+          <button onClick={deleteFact}>Delete Fact</button>
         </div>
       )
-    }
+   }
 }
 
 
